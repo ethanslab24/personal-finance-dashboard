2cbf6292ae9bcfadc5561cb038fe9f0f10643270
ethan <eduena4@wgu.edu>
2026-06-07 13:43:23 -0500

    Implemented transaction create and retrieval endpoints

--- src/main/java/com/ethan/personal_finance_dashboard/Transaction/TransactionController.java
+++ src/main/java/com/ethan/personal_finance_dashboard/Transaction/TransactionController.java
@@ -1,10 +1,17 @@
 package com.ethan.personal_finance_dashboard.Transaction;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/transactions")
@@ -20,4 +27,20 @@ public class TransactionController {
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> findTransactionById(@PathVariable Long id) {
+        Transaction transaction = transactionRepository.findById(id).orElse(null);
+        if (transaction == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(transaction);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@Valid @RequestBody Transaction transaction) {
+        Transaction savedTransaction = transactionRepository.save(transaction);
+        return ResponseEntity.created(URI.create("/transactions/" + savedTransaction.getId())).body(savedTransaction);
+    }
+
 }
