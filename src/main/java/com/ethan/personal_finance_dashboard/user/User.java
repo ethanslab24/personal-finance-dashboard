6cd233a756573748ae9bd76a7b5e7797b4c5aa12
ethan <eduena4@wgu.edu>
2026-06-18 12:36:34 -0500

    Added user entity

---
+++ src/main/java/com/ethan/personal_finance_dashboard/user/User.java
@@ -0,0 +1,45 @@
+package com.ethan.personal_finance_dashboard.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String username;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+}
---
+++ src/main/java/com/ethan/personal_finance_dashboard/user/UserRepository.java
@@ -0,0 +1,1 @@
+package com.ethan.personal_finance_dashboard.user;
